b52afd2bff882c330ef139500aa64e92e3c5737a
ykwu <ykwu@freewheel.com>
2019-12-18 15:59:00 +0800

    note

--- commit-to-git.sh
+++ commit-to-git.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-cd /Users/ykwu/Desktop/GitHub && git add -A && git commit -m 'note' && git push origin master:master
+cd /Users/ykwu/Desktop/GitHub && date && git add -A && git commit -m 'note' && git push origin master:master
