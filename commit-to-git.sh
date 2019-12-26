e2b2e298d24981fc70ef2e4f3c31d346944cfc5a
wuyuankun <573737693@qq.com>
2019-12-26 23:18:26 +0800

    note

--- commit-to-git.sh
+++ commit-to-git.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-cd /Users/ykwu/Desktop/GitHub && date && git add -A && git commit -m 'note' && git pull origin && git push origin master:master
+cd /Users/yuankunwu/Desktop/GitHub/doc && date && git add -A && git commit -m 'note' && git pull origin && git push origin master:master
